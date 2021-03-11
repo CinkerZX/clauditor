c44ccfb98c6fd78f5edc92608e396a540c193003
Reggie <cushingreggie@gmail.com>
2021-03-11 11:14:51 +0100

    switch to new actor abstraction

--- auditor.js
+++ auditor.js
@@ -1,5 +1,6 @@
 const cmdArgs = require('command-line-args')
-//const secureAmqp = require('../cllibsecureamqp')
+const SecureAmqp = require('../cllibsecureamqp').SecureAmqp
+const Actor = require('../cllibsecureamqp').Actor
 //const secureAmqp = require('secureamqp')
 
 const cmdOptions = [
@@ -17,50 +18,23 @@ const toAddress = options.send
 let domainToken = null
 
 async function main() {
-	await secureAmqp.init(config)
-	const myAddress = secureAmqp.getMyAddress()
-	const myId = secureAmqp.id
-	console.log("Actor address: ", myAddress)
-	console.log("Actor keys: ", secureAmqp.keys())
-	
-	// Sign my Id by domain
-	secureAmqp.callFunction(config.domain, ".f.signActorId", myId, null, function(res) {
-		const token = res.msg.response
-		const status = res.msg.status
-		const verify = secureAmqp.verifyToken(token)
-		const decoded = secureAmqp.decodeToken(token)
-		if(verify) {
-			console.log("Received domain token: ", token)
-			domainToken = token
-		}
+	const auditor = new Actor(config)
+	await auditor.boot()
+	auditor.listen("codeRed", function(event) {
+		console.log("Received event: ", event)
 	})
-
-	// Subscribe to all events
-	secureAmqp.subscribeEvent('*', function(e) { 
-		console.log("Received event: ", e)
-		// Do something
-	})
-
-	secureAmqp.monitorFunctions(function(m) {
-		console.log("Function: ",m)
-	})
-
-	secureAmqp.monitorEvents(function(m) {
-		console.log("Event: ",m)
-	})
-
-	// Sign requests
-	secureAmqp.registerFunction('.f.sign', null, function(req, res) {
+	auditor.createAbility('sign', true, function(req, res) {
 		console.log("Auditor received signing request: ", req.msg)
 		// Do auditor magic
 
-		// Add the domain authorization for this actor
 		const request = req.msg
-		request.domainToken = domainToken
+		console.log(request)
 		// Sign request
-		const token = secureAmqp.signedToken(request)
-		res.send(token)
+		///const token = auditor.secureAmqp.signedToken(request)
+		const token = auditor.sign(request)
+		res.send(token, 200)
 	})
+
 }
 
 main()
